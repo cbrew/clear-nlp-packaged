3ce4a24e493295c14b8361bff57eee221ff78d6a
Chris Brew <cbrew@ets.org>
2012-12-12 13:20:16 -0500

    now adds syntactic and semantic features to the .tsv file

--- tsvparse.py
+++ tsvparse.py
@@ -1,6 +1,13 @@
+"""
+Code to generate features from a module loaded
+on the command line and map it to a column in the .tsv file.
+"""
+
 import pandas
 import sys
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 
 features = __import__(sys.argv[1])
@@ -10,7 +17,13 @@ features.oninit()
 
 q = pandas.read_table(sys.argv[2])
 
+def control_length(text,limit=50000):
+    """
+    Ensure text is not too long. 
+    """
+    return text[:limit]
 
-q[sys.argv[1]] = q.text.map(features.featureize)
+
+q[sys.argv[1]] = q.text.map(lambda x: control_length(features.featureize(x)))
 q.to_csv(sys.argv[3],sep='\t',index=False)
 features.onexit()
